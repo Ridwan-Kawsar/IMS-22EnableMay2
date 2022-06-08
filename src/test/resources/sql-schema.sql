091690236a1657c6c69c7875f3acb0aa1ed5e338
Ridwan-Kawsar <ridwan_kawsar@hotmail.co.uk>
2022-06-09 00:09:45 +0100

    Third Commit, Updated SQL Schema

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,37 @@
-DROP TABLE IF EXISTS `customers`;
+DROP DATABASE IF EXISTS ims;
 
-CREATE TABLE IF NOT EXISTS `customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+CREATE DATABASE IF NOT EXISTS ims;
+
+USE ims; 
+
+CREATE TABLE IF NOT EXISTS customers (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(40) DEFAULT NULL,
+    surname VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    item_id INT(11) NOT NULL AUTO_INCREMENT,
+    item_name VARCHAR(40) DEFAULT NULL,
+    price DECIMAL(10,2) DEFAULT NULL,
+    PRIMARY KEY (item_id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+	order_id INT NOT NULL AUTO_INCREMENT, 
+    fk_id INT NOT NULL,
+	PRIMARY KEY (order_id),
+	FOREIGN KEY (fk_id) REFERENCES customers(id)
+);
+
+CREATE TABLE IF NOT EXISTS orders_items (
+	fk_order_id INT NOT NULL,
+	fk_item_id INT NOT NULL, 
+	orders_items_id INT NOT NULL AUTO_INCREMENT,
+    quantity INT DEFAULT 1, 
+    total_cost DECIMAL(10,2),
+	PRIMARY KEY (orders_items_id),
+	FOREIGN KEY (fk_order_id) REFERENCES orders(order_id),
+	FOREIGN KEY (fk_item_id) REFERENCES items(item_id)
 );
